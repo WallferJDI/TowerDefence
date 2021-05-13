fb5e168cf0649566d6049ddc612c3ee893fb3033
Wallfer <wallferjdi@gmail.com>
2021-05-13 19:29:56 +0300

    бахнул свой метод LookAt т.к встроенный очень резко и стремно переключался на другой обьект

--- Assets/Scripts/Turet.cs
+++ Assets/Scripts/Turet.cs
@@ -10,39 +10,52 @@ public class Turet : MonoBehaviour
     [SerializeField]
     private string enemyTag = "Enemy";
 
-    public GameObject turetUp;
+    public float rotationSpeed = 10f;
+
+
+    public Transform partForRotation;
     void Start()
     {
         InvokeRepeating("searchTarget", 0f, 0.5f);
+    }
+    
+     void Update()
+    {
+
+        LookAt();
+       
+
+        //turetUp.transform.LookAt(target);
+    }
+    void LookAt()
+    {
+        if (target == null)
+            return;
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partForRotation.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
+        partForRotation.rotation = Quaternion.Euler(0f, rotation.y, 0f);
     }
     void searchTarget()
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
         float shortestDistance = Mathf.Infinity;
         GameObject nearestEnemy = null;
-         foreach(GameObject enemy in enemies)
-         {
+        foreach (GameObject enemy in enemies)
+        {
             float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
             if (distanceToEnemy < shortestDistance)
             {
                 shortestDistance = distanceToEnemy;
                 nearestEnemy = enemy;
             }
-         }
+        }
         if (nearestEnemy != null && shortestDistance <= range)
             target = nearestEnemy.transform;
         else
             target = null;
 
-        
-    }
-     void Update()
-    {
-        if (target == null)
-            return;
 
-
-        //turetUp.transform.LookAt(target);
     }
     private void OnDrawGizmosSelected()
     {
